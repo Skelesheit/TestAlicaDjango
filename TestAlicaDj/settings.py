dd6dfe94bc6a267d8b1adafd4075a786bf41e62b
Skelesheit <Skelesheit@gmail.com>
2025-04-19 21:19:51 +0300

    добавлена ORM и зависимости

--- TestAlicaDj/settings.py
+++ TestAlicaDj/settings.py
@@ -98,8 +98,12 @@ SPECTACULAR_SETTINGS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alice_chat',
+        'USER': 'skelesheit',
+        'PASSWORD': 'S1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- test_alice/models.py
+++ test_alice/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+class Session(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_open = models.BooleanField(default=False)
+
+class ChatHistory(models.Model):
+    message = models.TextField()
+    role = models.CharField(max_length=100)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
